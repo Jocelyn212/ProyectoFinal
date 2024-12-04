2e1bc073a3fe9db0aeaba49b05624bd305d9c3d6
Your NameCristiana <papercri@gmail.com>
2024-12-04 21:31:56 +0100

    styles for cart

--- femShop/src/views/Home.vue
+++ femShop/src/views/Home.vue
@@ -5,7 +5,7 @@
         <form class="self-center mb-8">
             <label for="pr-search" class="hidden">Search</label>
             <input  id="pr-search"  type="text" placeholder="Search ..." class="form-input">
-            <button class="button button-primary -ml-[8px] relative">Search</button>
+            <button class="button button-primary -ml-[12px] relative">Search</button>
         </form>
         <div class="shop-container">
             <Card v-for="product in products" :product="product" @click=""/>
