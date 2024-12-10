31dd96e231be75599aa58e7ad23d4665c5b5f075
Jocelyn Castro <jocelyn.cf@gmail.com>
2024-12-10 14:09:37 +0100

    agregada la busqueda

--- femShop/src/views/Home.vue
+++ femShop/src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
    <main>
     <div class="container flex-col">
         <h2 v-if="products" class="title-1 mt-10">Category: {{ products[0].category.name }}</h2>
@@ -47,4 +47,66 @@ export default {
 </script>
 <style>
 
+</style> -->
+<template>
+   <main>
+    <div class="container flex-col">
+        <h2 v-if="products" class="title-1 mt-10">Category: {{ products[0].category.name }}</h2>
+        <form class="self-center mb-8" @submit.prevent="searchProducts">
+            <label for="pr-search" class="hidden">Search</label>
+            <input v-model="searchQuery" id="pr-search" type="text" placeholder="Search ..." class="form-input" />
+            <button class="button button-primary -ml-[12px] relative">Search</button>
+        </form>
+        <div class="shop-container">
+            <Card v-for="product in filteredProducts" :key="product.id" :product="product" @click=""/>
+        </div>
+    </div>
+   </main>
+</template>
+
+<script>
+import axios from "axios";
+import Card from "../components/Card.vue";
+
+export default {
+    name: "Home",
+    components: {
+        Card
+    },
+    data() {
+        return {
+            products: null,
+            searchQuery: "",
+            filteredProducts: null,
+        }
+    },
+    methods: {
+        async getProductData() {
+            try {
+                const response = await axios.get(`https://api.escuelajs.co/api/v1/products/?categoryId=5`);
+                this.products = response.data;
+                this.filteredProducts = this.products; // Inicialmente, mostrar todos los productos
+            } catch(error) {
+                console.log(error)
+            }
+        },
+        searchProducts() {
+            if (!this.searchQuery) {
+                this.filteredProducts = this.products;
+            } else {
+                this.filteredProducts = this.products.filter(product =>
+                    product.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+                );
+            }
+            this.searchQuery = "";
+        }
+    },
+    mounted() {
+        this.getProductData();
+    }
+}
+</script>
+
+<style>
+/* Agrega tus estilos aquí */
 </style>
