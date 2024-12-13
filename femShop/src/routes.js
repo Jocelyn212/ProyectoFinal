fe1306fd9a418ad1ab566b667906ce52765b3b0b
dperezca <54083655+dperezca@users.noreply.github.com>
2024-12-13 20:20:10 +0100

    Footer, búsqueda por query (producto y categoria), spinner, mensaje si no hay productos

--- femShop/src/routes.js
+++ femShop/src/routes.js
@@ -5,6 +5,7 @@ import Cart from "./views/Cart.vue";
 
 export const routes = [
   { path: "/", name: "home", component: Home },
+  { path: "/search", name: "search", component: Home },
   { path: "/product/:id", name: "product", component: Product },
   { path: "/cart", name: "cart", component: Cart },
   { path: "/:path(.*)", component: NotFound },
