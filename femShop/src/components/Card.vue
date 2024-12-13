5a39002074c68ad858e8b9b1def3e1aa7630ed1f
Jocelyn Castro <jocelyn.cf@gmail.com>
2024-12-13 13:04:47 +0100

    agragado manejo de favoritos en card

--- femShop/src/components/Card.vue
+++ femShop/src/components/Card.vue
@@ -1,36 +1,151 @@
-<template>
-    <div class="card">
-        <figure class="relative"><img v-if="product.images" :src="product.images[0]" :alt="product.title">
-        <span class="favorite fa-regular fa-heart"></span>
-        <span class="favorite fa-solid fa-heart"></span>
-        </figure>
-        <div class="product-details">
-            <h3 class="title flex items-center">{{ product.title}}</h3>
-            <p class="price">€ {{ product.price}}</p>
-        </div>
-        <div class="buttons flex  p-1 justify-between items-end">    
-            <button class="button button-secondary whitespace-nowrap mr-2 rounded-xl " @click="cartStore.addItemToCart(product,1)"> Add to cart </button>
-           <button class="button button-secondary rounded-xl " @click="showProductDetails"><span class="fa-regular fa-eye"></span></button>
-        </div>
+<!-- <template>
+  <div class="card">
+    <figure class="relative">
+      <img
+        v-if="product.images"
+        :src="product.images[0]"
+        :alt="product.title"
+      />
+
+      <button
+        class="button button-icon"
+        @click="toggleFavorite"
+        :title="
+          favoritesStore.isFavorite(product.id)
+            ? 'Remove from favorites'
+            : 'Add to favorites'
+        "
+      >
+        <span
+          :class="[
+            'fa-heart text-2xl',
+            favoritesStore.isFavorite(product.id)
+              ? 'fa-solid text-red'
+              : 'fa-regular text-dark-grey hover:text-red',
+          ]"
+        ></span>
+      </button>
+    </figure>
+    <div class="product-details">
+      <h3 class="title flex items-center">{{ product.title }}</h3>
+      <p class="price">€ {{ product.price }}</p>
     </div>
+    <div class="buttons flex p-1 justify-between items-end">
+      <button
+        class="button button-secondary whitespace-nowrap mr-2 rounded-xl"
+        @click="cartStore.addItemToCart(product, 1)"
+      >
+        Add to cart
+      </button>
+      <button
+        class="button button-secondary rounded-xl"
+        @click="showProductDetails"
+      >
+        <span class="fa-regular fa-eye"></span>
+      </button>
+    </div>
+  </div>
 </template>
 <script>
-import { useCartStore} from "../stores/cart"
-import {mapStores} from "pinia"
+import { useCartStore } from "../stores/cart";
+import { mapStores } from "pinia";
 
 export default {
-    name: "Card",
-    props: ["product"],
+  name: "Card",
+  props: ["product"],
 
-    computed:{
-    ...mapStores(useCartStore)
-},
-methods: {
-        showProductDetails() {
-            this.$router.push(`/product/${this.product.id}`)
-        }
-    }
-
-}
+  computed: {
+    ...mapStores(useCartStore),
+  },
+  methods: {
+    showProductDetails() {
+      this.$router.push(`/product/${this.product.id}`);
+    },
+  },
+};
 </script>
-<style></style>
+<style></style> -->
+<template>
+  <div class="card">
+    <figure class="relative">
+      <img
+        v-if="product.images"
+        :src="product.images[0]"
+        :alt="product.title"
+      />
+      <button
+        class="button button-icon"
+        @click="handleFavoriteClick"
+        :title="
+          favoritesStore.isFavorite(product.id)
+            ? 'Remove from favorites'
+            : 'Add to favorites'
+        "
+      >
+        <span
+          :class="[
+            'fa-heart text-2xl',
+            favoritesStore.isFavorite(product.id)
+              ? 'fa-solid text-red'
+              : 'fa-regular text-dark-grey hover:text-red',
+          ]"
+        ></span>
+      </button>
+    </figure>
+    <div class="product-details">
+      <h3 class="title flex items-center">{{ product.title }}</h3>
+      <p class="price">€ {{ product.price }}</p>
+    </div>
+    <div class="buttons flex p-1 justify-between items-end">
+      <button
+        class="button button-secondary whitespace-nowrap mr-2 rounded-xl"
+        @click="cartStore.addItemToCart(product, 1)"
+      >
+        Add to cart
+      </button>
+      <button
+        class="button button-secondary rounded-xl"
+        @click="showProductDetails"
+      >
+        <span class="fa-regular fa-eye"></span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useCartStore } from "../stores/cart";
+import { useFavoritesStore } from "../stores/favorites";
+import { useActiveUserStore } from "../stores/user";
+import { mapStores } from "pinia";
+
+export default {
+  name: "Card",
+  props: ["product"],
+
+  computed: {
+    ...mapStores(useCartStore, useFavoritesStore, useActiveUserStore),
+  },
+
+  methods: {
+    showProductDetails() {
+      this.$router.push(`/product/${this.product.id}`);
+    },
+    handleFavoriteClick() {
+      if (!this.activeUserStore.profile) {
+        alert("Please login to add favorites");
+        return;
+      }
+      this.favoritesStore.toggleFavorite(this.product);
+    },
+  },
+
+  async mounted() {
+    if (this.activeUserStore.profile) {
+      await this.favoritesStore.getFavoritesFromFirebase();
+    } else {
+      this.favoritesStore.getLocalStorageFavorites();
+    }
+  },
+};
+</script>
