2b7f83b8532488213dfd4dce891e4db43ed35137
Your NameCristiana <papercri@gmail.com>
2024-12-16 12:02:40 +0100

    Not Found

--- femShop/src/views/NotFound.vue
+++ femShop/src/views/NotFound.vue
@@ -1,7 +1,11 @@
 <template>
-<main>
-    <div class="container flex justify-center items-center">
-        <h2 class="title-1">404. Not Found</h2>
+<main class="h-full">
+    <div class="container flex flex-wrap justify-center items-center h-[40vh] ">
+        <h2 class="title-1 !text-9xl !font-normal w-full text-center mt-10">404</h2>
+        <span class="title-1 !text-4xl mt-[-20px] w-full text-center">Not Found</span>
+        <RouterLink to="/shop" >
+            <button class="button button-secondary mt-10">Take me back to FemShop</button>
+        </RouterLink>
     </div>
 </main>
  </template>
