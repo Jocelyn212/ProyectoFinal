4f200bcc38ba75afbb33986e094a7aeb4fbdef22
Your NameCristiana <papercri@gmail.com>
2024-12-16 10:25:13 +0100

    names

--- femShop/src/routes.js
+++ femShop/src/routes.js
@@ -1,12 +1,12 @@
+import Landing from "./views/Landing.vue";
 import Home from "./views/Home.vue";
-import Shop from "./views/Shop.vue";
 import NotFound from "./views/NotFound.vue";
 import Product from "./views/Product.vue";
 import Cart from "./views/Cart.vue";
 
 export const routes = [
-  { path: "/", name: "home", component: Home },
-  { path: "/shop", name: "shop", component: Shop },
+  { path: "/", name: "landing", component: Landing },
+  { path: "/shop", name: "home", component: Home },
   { path: "/product/:id", name: "product", component: Product },
   { path: "/cart", name: "cart", component: Cart },
   { path: "/:path(.*)", component: NotFound },
--- femShop/src/views/Home.vue
+++ femShop/src/views/Home.vue
@@ -1,67 +1,111 @@
 <template>
-    <main class="w-full h-screen p-0 m-0 absolute top-0 left-0">
-        <div class="home w-full h-screen">
-            <div  class="bg">
-                <div class="shop-now container  h-screen flex flex-wrap relative items-center content-center text-white font-bold gap-8 drop-shadow-md !max-w-screen-md">      
-                    <h2 class="w-full text-center sm:text-6xl text-5xl uppercase ">
-                        Our Black Friday sale is finally here!
-                    </h2>
-                    <p class="w-full text-center sm:text-5xl text-4xl">50% off sitewide</p>
-                    <div  class="w-full text-center mt-8">
-                        <RouterLink to="/shop">
-                            <buton class="button button-white text-3xl uppercase !text-white !border-lila-primary hover:bg-lila-primary">Shop now</buton>
-                        </RouterLink>
-                    </div>
-                    
+    <main>
+        <Breadcrumb :categoryName="selectedCategory ? selectedCategory.name : ''" />
+        <div class="container flex-col relative">      
+            <h2 v-if="products" class="title-1 text-center mt-4 relative z-20 sm:block hidden">{{ selectedCategory ? selectedCategory.name : 'All products' }}</h2>
+
+            <div class="catalog-headings">
+            <!-- Dropdown categorias -->
+                <div class="relative inline-block w-[70%] sm:w-auto z-20 ">
+                    <button class="dropdown-button w-full " @click="toggleMenu">Browse categories 
+                        <span v-if="CatisOpen" class="fa-solid fa-chevron-up ml-2 text-dark-grey"></span>
+                        <span  v-else class="fa-solid fa-chevron-down ml-2 text-dark-grey"></span>
+                    </button>
+                    <ul v-if="CatisOpen" class="dropdown-menu" @click="toggleMenu">
+                        <li @click="filterByCategory(null)" >Al products</li>
+                        <li v-for="category in categories" :key="category.id" @click="filterByCategory(category)" >{{ category.name }}</li>
+                    </ul>
                 </div>
+            
+            <!-- Search -->
+                <form class="self-center mb-0 z-10 relative" @submit.prevent="searchProducts">
+                    <label for="pr-search" class="hidden">Search</label>
+                    <input v-model="searchQuery" id="pr-search" type="text" placeholder="Search ..." class="form-input sm:w-[220px] !border-amarillo" />
+                    <button class="button button-primary -ml-[12px] relative !border-amarillo">Search</button>
+                </form>
+            <!-- Page title -->  
+                <h2 v-if="products" class="title-2 text-center pb-2 relative  sm:hidden block">{{ selectedCategory ? selectedCategory.name : 'All products' }}</h2>
+            </div>
+            <!-- Shop -->
+            <div class="shop-container">
+                <Card v-for="product in filteredProducts" :key="product.id" :product="product" @click=""/>
             </div>
         </div>
     </main>
  </template>
  
  <script>
- 
+ import axios from "axios";
+ import Breadcrumb from "../components/Breadcrumb.vue";
+ import Card from "../components/Card.vue";
+ import { getAllProducts } from "../firebase"; // Asegúrate de importar el método correcto
  
  export default {
-     name: "Home",
+     name: "Shop",
+     components: {
+         Card, Breadcrumb
+     },
+     data() {
+         return {
+             CatisOpen: false, 
+             products: null,
+             searchQuery: "",
+             filteredProducts: null,
+             categories: [], // Lista de categorías
+             selectedCategory: null, // Categoría seleccionada
+         }
+     },
+     methods: {
+         async getProductData() {
+             try {
+                 const response = await getAllProducts();
+                 this.products = response;
+                 this.filteredProducts = this.products; 
+                 this.extractCategories(); // Extraer categorías de los productos
+             } catch(error) {
+                 console.log(error)
+             }
+         },
+         searchProducts() {
+             this.filterProducts();
+             this.searchQuery = ""; // Borrar el campo de búsqueda
+         },
+         filterProducts() {
+             let filtered = this.products;
+             if (this.selectedCategory) {
+                 filtered = filtered.filter(product => product.category.id === this.selectedCategory.id);
+             }
+             if (this.searchQuery) {
+                 filtered = filtered.filter(product =>
+                     product.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+                 );
+             }
+             this.filteredProducts = filtered;
+         },
+         filterByCategory(category) {
+             this.selectedCategory = category;
+             this.filterProducts();
+         },
+         extractCategories() {
+             const categories = this.products.map(product => product.category);
+             this.categories = [...new Set(categories.map(category => JSON.stringify(category)))].map(category => JSON.parse(category));
+         },
+         toggleMenu() {
+            this.CatisOpen = !this.CatisOpen;
+        },
+        
+     },
+     watch: {
+        selectedCategory(newCategory) {
+            this.$root.$emit( newCategory ? newCategory.name : 'All Products');
+        },
+    },
+     mounted() {
+         this.getProductData();
+     }
  }
  </script>
  
  <style>
-   .home .home{
-        width:100%;
-        height: 100vh;
-        background:  url('../img/home3.webp') no-repeat center/cover;
-        position:relative;
-        
-    }
-    .home header > div.container.flex{
-        display:none
-    }
-    .home header .header-gradient{
-        background-color: #540D6E!important;
-        background-image: none!important;
-        box-shadow: none!important;
-    }
-    .home .bg{
-        background-color: #00000060;
-    }
-    .home footer{
-        display:none!important
-    }
-    .home  .shop-now{
-        -webkit-text-stroke: 3px #540D6E;
-        text-shadow: 0 0 0 #232323;
-        transition: all 250ms;
-    }
-    .home .shop-now a{
-        -webkit-text-stroke: 1.5px #540D6E;
-        outline:1.5px!important;
-        text-decoration:none
-    }
-    @media (min-width: 640px) {
-        .home header .logo img {
-            width: 160px;
-        }
-    }
+
  </style>
