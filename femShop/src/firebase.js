28911a6b50c7ba2da2d669bd176268d4dad1ba40
dperezca <54083655+dperezca@users.noreply.github.com>
2024-12-17 12:57:07 +0100

    Bugs y cositas

--- femShop/src/firebase.js
+++ femShop/src/firebase.js
@@ -1,20 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { getDatabase, ref, set, get, child } from "firebase/database";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
 
-/* const firebaseConfig = {
-  apiKey: "AIzaSyCWOoXFv8MTNA-A8IAEcGHu7YRcJkU8VxE",
-  authDomain: "shop-1656d.firebaseapp.com",
-  databaseURL: "https://shop-1656d-default-rtdb.firebaseio.com",
-  projectId: "shop-1656d",
-  storageBucket: "shop-1656d.firebasestorage.app",
-  messagingSenderId: "420163986032",
-  appId: "1:420163986032:web:6007bc8b7166478eee8ab5",
-}; */
-  const firebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
@@ -24,25 +11,6 @@ import { initializeApp } from "firebase/app";
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const firebaseConfig2 = {
-  apiKey: "AIzaSyCj6ME5O_gIrVHGqNgZBc7_S2dGz0mdXFg",
-  authDomain: "test-8cff7.firebaseapp.com",
-  databaseURL: "https://test-8cff7-default-rtdb.firebaseio.com",
-  projectId: "test-8cff7",
-  storageBucket: "test-8cff7.firebasestorage.app",
-  messagingSenderId: "576112958325",
-  appId: "1:576112958325:web:bc3b3d5a39c6516fd1913c",
-};
-const firebaseConfig3 = {
-  apiKey: "AIzaSyC67X4TZZiSuKMOu5AneWXL2QWFLtUmVrk",
-  authDomain: "cris-aa1f3.firebaseapp.com",
-  databaseURL:
-    "https://cris-aa1f3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cris-aa1f3",
-  storageBucket: "cris-aa1f3.firebasestorage.app",
-  messagingSenderId: "76141387735",
-  appId: "1:76141387735:web:1aefce9328e926b7b1b592",
-};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Get a reference to the database service
@@ -60,10 +28,8 @@ export async function getCarts(userId) {
   try {
     const snapshot = await get(child(dbRef, `carts/${userId}`));
     if (snapshot.exists()) {
-      console.log(snapshot.val());
       return snapshot.val();
     } else {
-      console.log("No data available");
       return [];
     }
   } catch (error) {
@@ -81,7 +47,6 @@ export async function getProduct(productId) {
       const product = products.find((product) => product.id == productId);
       return product;
     } else {
-      console.log("No data available");
       return null;
     }
   } catch (error) {
@@ -96,12 +61,8 @@ export async function getAllProducts() {
     const snapshot = await get(child(dbRef, `productos`));
     console.log("snap", snapshot);
     if (snapshot.exists()) {
-      console.log(snapshot.val());
-      console.error("error");
       return snapshot.val();
     } else {
-      console.log("No data available");
-      console.error("error", error);
       return [];
     }
   } catch (error) {
